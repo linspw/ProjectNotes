610a6f608c91d9af01db3cf666ebab2b4d8f5809
Jessé <linspw@hotmail.com>
2019-04-25 16:02:19 -0300

    Organizando Estrutura

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router, Route, Switch } from "react-router";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
