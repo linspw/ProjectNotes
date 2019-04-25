abc8254a2e340ce3ee22f5da4e8063260da241b0
Jessé <linspw@hotmail.com>
2019-04-25 13:28:52 -0300

    Updates

---
+++ precache-manifest.d2ae1fbe4f94a602a3dfcb3cb7af73c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7354a3fa7ac54f4bec5316a314771255",
+    "url": "/ProjectNotes/index.html"
+  },
+  {
+    "revision": "428733d8385594017611",
+    "url": "/ProjectNotes/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ProjectNotes/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "428733d8385594017611",
+    "url": "/ProjectNotes/static/js/main.f24d2fbc.chunk.js"
+  },
+  {
+    "revision": "665c401aaefcfbf0e3d7",
+    "url": "/ProjectNotes/static/js/runtime~main.8b4931c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ProjectNotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
